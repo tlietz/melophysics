9464182fafb6dce6d258c58af836233f6ac8e4cb
tlietz <theodore.lietz@gmail.com>
2023-09-21 09:40:02 -0500

    makes testing more fun

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Music made with physics simulations.
 
 Uses [Rapier](https://rapier.rs/) for simulations because it is cross-platform deterministic, and it has javascript bindings.
 
+## Setup
 ```bash
 npm i
 npm run start
--- src/Game.ts
+++ src/Game.ts
@@ -5,5 +5,6 @@ export const handlePress = (graphic: PIXI.Graphics, event: PIXI.InteractionEvent
   console.log(graphic.position)
   console.log(event.data)
   console.log(rigidBody)
-  rigidBody.applyImpulse({ x: 0, y: -2000000 }, true)
+  Math.round(Math.random())
+  rigidBody.applyImpulse({ x: 0, y: 1000000 * (Math.random() > 0.5 ? 1 : -1) }, true)
 }
